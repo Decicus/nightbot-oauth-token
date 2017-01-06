e865d51e8a9b9690d05b29c281677f4af16c3f78
Alex Thomassen <alex@thomassen.xyz>
2017-01-06 11:16:41 +0100

    Allow revoking the token after authenticating

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var scopes = {
     "song_requests_playlist": "View, add, edit, and remove songs on the channel song request playlist",
     "spam_protection": "View, edit, enable, and disable channel spam protection filters",
     "subscribers": "View, add, and remove channel subscribers (useful for third party subscription services)",
-    "timers": "View, add, edit, and remove channel timers"    
+    "timers": "View, add, edit, and remove channel timers"
 };
 var params = {};
 
@@ -19,32 +19,32 @@ $(document).ready(function() {
         connect = $('.connect'),
         token = $('.token'),
         hash = window.location.hash.replace('#', '');
-        
+
     container.attr('size', Object.keys(scopes).length);
     var split = hash.split("&");
-    
+
     if (split.length > 0) {
         $.each(split, function(index, value) {
             var param = value.split("=");
             params[param[0]] = decodeURIComponent(param[1]);
         });
     }
-    
+
     if (params.scope) {
         params.scope = params.scope.split(" ");
     }
-    
+
     $.each(scopes, function(scope, description) {
         $('<option/>')
             .attr('id', scope)
             .html(scope + ' &mdash; ' + description)
             .appendTo(container);
-            
+
         if (params.scope && params.scope.indexOf(scope) >= 0) {
             $('#' + scope, container).attr('selected', "selected");
         }
     });
-    
+
     if (params.access_token) {
         connect.hide();
         $('.well', token).html(params.access_token);
@@ -55,7 +55,7 @@ $(document).ready(function() {
                     .html(scope + ' &mdash; ' + scopes[scope])
             );
         });
-        
+
         $.ajax({
             url: 'https://api.nightbot.tv/1/me?access_token=' + params.access_token,
             type: 'GET',
@@ -68,22 +68,35 @@ $(document).ready(function() {
                     var expires = new Date(data.authorization.credentials.expires);
                     $('.date').html(expires.toLocaleString());
                 }
+            },
+            error: function() {
+                $('.date').html('An error occurred retrieving the token expiry date.');
             }
-        });        
+        });
     }
-    
+
     $('.connect a').on('click', function() {
         var auth = [];
         $(':checked', container).each(function() {
             var scope = $(this).attr('id');
             auth.push(scope);
         });
-        
+
         var authUrl = 'https://api.nightbot.tv/oauth2/authorize?response_type=token';
         authUrl += '&client_id=' + clientId;
         authUrl += '&redirect_uri=' + encodeURIComponent(redirectUri);
         authUrl += '&scope=' + auth.join('+');
-        
+
         window.location.href = authUrl;
     });
+
+    if (params.access_token) {
+        $('.logout').on('click', function() {
+            $.post('https://api.nightbot.tv/oauth2/token/revoke', {
+                token: params.access_token
+            });
+            connect.show();
+            token.hide();
+        });
+    }
 });
